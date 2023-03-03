f028964dc377b8221ee1e641d27878476a222b99
Aaditi <aaditimishra9595@gmail.com>
2023-03-03 18:41:39 +0530

    adding to development branch

--- gitdemo.py
+++ gitdemo.py
@@ -3,14 +3,8 @@ print("ineuron")
 print("line3")
 print("line 3 by shashank")
 print("line 5 in Github")
-<<<<<<< HEAD
 print("line 6 on local")
 print("line 7 on local")
 print("line 8 on local")
 print("line 9 on local")
-=======
-print("line 6 on github")
-print("line 7 on github")
-print("line 8 on github")
-print("line 9 on github")
->>>>>>> 645ea3b08db72efd416cb6999ef0b9f7208827a4
+print("added")
